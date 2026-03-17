4daba5d3c0f9c0f685325bd119318cc5023e32ac
24dce101-cmd <24dce101@charusat.edu.in>
2026-03-17 22:27:18 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
+
 
 export default function App() {
-          return <BrowserRouter>
+          return (
+          <BrowserRouter>
+           <Header />
                     <Routes>
                       <Route path="/" element={<Home />} />
                       <Route path="/Sign-in" element={<SignIn />} />
-                      <Route path="/Sign-out" element={<SignUp />} />
+                      <Route path="/Sign-up" element={<SignUp />} />
                       <Route path="/About" element={<About />} />
                       <Route path="/Profile" element={<Profile />} />
                     </Routes>
                   </BrowserRouter>
+          );
         }
 
